e71c02e7930dbb48627dc3f98d3c3ff2ed87d440
bhavuk sharma <bhavuk55555@gmail.com>
2025-07-07 03:49:26 +0530

    opertors example

---
+++ java-operators/ArithmeticOperatorsExample.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperatorsExample{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+
+        // Artithematic operations
+        System.out.println("Addition: " + (a+b));
+        System.out.println("Subtraction: " + (a-b));
+        System.out.println("Multiplication: " + (a*b));
+        System.out.println("Division: " + (a/b));
+        System.out.println("Modulus: " + (a%b));
+        System.out.println("PreIncrement: " + (++a));
+        System.out.println("PostIncrement: " + (b++));
+        System.out.println("PreDecrement: " + (--a));
+        System.out.println("PostDecrement: " + (b--));
+
+    }
+}
---
+++ java-operators/ComparisonOperatorsExample.java
@@ -0,0 +1,17 @@
+public class ComparisonOperatorsExample {
+    public static void main(String[] args){
+        // Declare two interger variables
+        int a = 10;
+        int b = 20;
+
+        System.out.println("values of a and b:"+ " a = " + a + ", b = " + b);
+
+        // Using comparison operators
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <=b: " + (a <= b));
+    }
+}
